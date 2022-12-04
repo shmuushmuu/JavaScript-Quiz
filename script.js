87009661640979841cc9e81bc8baa4eef03b22b7
Sam Craig <samfcraig@gmail.com>
2022-12-04 14:56:35 -0500

    added values to the questions array

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ var nextEl = document.querySelector('#next');
 var cursor = 0;
 
 var questions = [
-    "",
-    "",
-    "",
-    "",
+    "Which of the following is not a class in Dungeons and Dragons 5th Edition?",
+    "Which of the following is not a damage type in D&D 5e?",
+    "Which of the following is not a school of magic in D&D 5e?",
+    "What is the maximum amount magical items a player character can attune to in D&D 5e?",
 ];
 
 var answers = ["a, b, c, d"];
