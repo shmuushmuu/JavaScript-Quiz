9ed4102bd8881a5c90c89d87634352ae70eecc04
Sam Craig <samfcraig@gmail.com>
2022-12-05 22:46:43 -0500

    query selectors should now be squared away

--- script.js
+++ script.js
@@ -2,75 +2,12 @@ var nextEl = document.querySelector('#next');
 var timerEl = document.querySelector('.countdown');
 var startEl = document.querySelector('#startButton');
 var quizEl = document.querySelector('.quiz');
-var answerA = document.getElementById("a");
-var answerB = document.getElementById("b");
-var answerC = document.getElementById("c");
-var answerD = document.getElementById("d");
+var gameOver = document.querySelector('.gameOver');
+var questions = document.querySelectorAll('.question');
 
 var cursor = 0;
 var secondsLeft = 75;
-var questions = [  {
-        Headline: "Which of the following is not a class in D&D 5e?",
-        answers: {
-            a: "Ranger",
-            b: "Bard",
-            c: "Artificer",
-            d: "Priest",
-        },
-        correct: "d",
-    },
-    {
-        Headline: "Which of the following is not an attribute in D&D 5e?",
-        answers: {
-            a: "Charisma",
-            b: "Power",
-            c: "Constitution",
-            d: "Wisdom",
-        },
-        correct: "b",
-    },
-    {
-        Headline: "What does DM inspiration do?",
-        answers: {
-            a: "It gives you advantage on a roll of your choice.",
-            b: "It guarantees that you succeed on every saving throw for the next round of combat.",
-            c: "It gives a +10 to your next attack.",
-            d: "Sam, DM inspiration isn't a thing. Loser.",
-        },
-        correct: "a",
-    },    
-    {
-        Headline: "Which subrace of elf gets a +1 to their charisma?",
-        answers: {
-            a: "High Elf",
-            b: "Wood Elf",
-            c: "Dark Elf",
-            d: "Sam, no elves get a +1 to charisma. Idiot.",
-        },
-        correct: "c",
-    },
-    {
-        Headline: "What dice would you roll for an athletics check?",
-        answers: {
-            a: "d12",
-            b: "d20",
-            c: "d100",
-            d: "Sam, you don't roll for an athletics check. Dork.",
-        },
-        correct: "b",
-    },
-    {
-        Headline: "What following monster are you actually able to play?",
-        answers: {
-            a: "Dragon",
-            b: "Werewolf",
-            c: "Lich",
-            d: "Sam, you can play whatever you and your DM agree upon... but man, you're handsome. Wow."
-        },
-        correct: "d",
-    }
-    ];
-
+var answers = ["a", "b", "a", "c", "b", "d"]
     
 var advance = function() {
     displayQuestion();
