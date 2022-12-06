366b47e1d57e3d43cdcae300ff4c9afd7c836951
Sam Craig <samfcraig@gmail.com>
2022-12-05 19:46:15 -0500

    I can't get my questions to show up. I'm dying.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var nextEl = document.querySelector('.next');
-var timerEl = document.querySelector('.countdown');
-var buttonEl = document.querySelector('#possible');
+var nextEl = document.querySelector('#next');
+var timerEl = document.querySelector('#countdown');
+var buttonEl = document.querySelector('#answers');
 var startEl = document.querySelector('#start');
 var cursor = 0;
 var secondsLeft = 75;
@@ -67,41 +67,40 @@ var questions = [
     }
     ];
 
-// function displayTime() {
-//     var label = "seconds";
-//     if (secondsLeft === 1) {
-//         label = "second";
-//     } 
-//     timerEl.textContent = secondsLeft + " " + label + "left on the clock!";
-// }
-// for displayTime() {
-// function countdown() {
-//     displayTime(secondsLeft);
-//     var timeInterval = setInterval(function() {
-//         secondsLeft--;
-//         displayTime(secondsLeft);
+function displayTime() {
+    var label = "seconds";
+    if (secondsLeft === 1) {
+        label = "second";
+    } 
+    timerEl.textContent = secondsLeft + " " + label + "left on the clock!";
+}
+function countdown() {
+    displayTime(secondsLeft);
+    var timeInterval = setInterval(function() {
+        secondsLeft--;
+        displayTime(secondsLeft);
     
-//         if (secondsLeft <= 0) {
-//             clearInterval(timeInterval);
-//             alert("TIME'S UP!");
-//     }
-//   }, 1000);
-// }
-// }
+        if (secondsLeft <= 0) {
+            clearInterval(timeInterval);
+            alert("TIME'S UP!");
+    }
+  }, 1000);
+}
+
+
+startEl.addEventListener('click', advance);
 
 var displayQuestion = function() {
-    nextEl.textContent = questions[cursor];
+    nextEl.textContent = Headline[cursor];
 }
 
 var advance = function() {
+    displayQuestion();
+    displayTime();
     if (cursor < questions.length -1) {
         cursor++;
     }
-    displayQuestion();
-    displayTime();
 };
-
-nextEl.addEventListener('click', advance);
 
 //buttonContainer.addEventListener('click, function(event)') {
 //    var answers = event.target;
